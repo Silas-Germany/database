815697f15e4e28b01a9d3545a05365391a47dcd3
Silas-Germany <silas.ne.india@gmail.com>
2019-01-07 23:34:16 +0100

    Create database and cursor classes

---
+++ complexorm/src/main/kotlin/com/github/silasgermany/complexorm/SqlDatabase.kt
@@ -0,0 +1,36 @@
+package com.github.silasgermany.complexorm
+
+import android.annotation.SuppressLint
+import android.database.CrossProcessCursor
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+
+class SqlDatabase(private val sqlDatabase: SQLiteDatabase) {
+
+    val requestsWithColumnInfo = false
+
+    @SuppressLint("Recycle")
+    fun queryForEach(sql: String, f: (Cursor) -> Unit): Unit = sqlDatabase.rawQuery(sql, null).run {
+        (this as? CrossProcessCursor)?.let { SqlCursor(it, requestsWithColumnInfo) }?.takeIf { it.valid } ?: this
+    }.run {
+        moveToFirst()
+        (0 until count).forEach { _-> f(this); moveToNext() }
+        close()
+    }
+
+    @SuppressLint("Recycle")
+    fun <T> queryMap(sql: String, f: (Cursor) -> T): List<T> = sqlDatabase.rawQuery(sql, null).run {
+        (this as? CrossProcessCursor)?.let { SqlCursor(it, requestsWithColumnInfo) }?.takeIf { it.valid } ?: this
+    }.run {
+        moveToFirst()
+        (0 until count).map { _-> f(this).also { moveToNext() } }.also { close() }
+    }
+
+    @SuppressLint("Recycle")
+    fun query(sql: String): Cursor = sqlDatabase.rawQuery(sql, null).run {
+        (this as? CrossProcessCursor)?.let { SqlCursor(it, requestsWithColumnInfo) }?.takeIf { it.valid } ?: this
+    }
+
+    fun rawSql(sql: String) = sqlDatabase.execSQL(sql)
+
+}
