f02fc3d21b92acd858c7c42fb2dedea1986fffb3
user153910 <user153910@MU209.local>
2019-08-17 19:06:59 +0530

    Saving changes unchecked

---
+++ complexOrmApi/src/iosMain/kotlin/com/github/silasgermany/complexormapi/Actual.kt
@@ -0,0 +1,45 @@
+package com.github.silasgermany.complexormapi
+
+import kotlin.experimental.and
+import kotlin.reflect.KClass
+
+actual class IdType(val bytes: ByteArray) {
+
+    init {
+        if (bytes.size != 16) throw IllegalArgumentException("Wrong byte size (${bytes.size})")
+    }
+
+    actual fun nicePrint(): String {
+        val hexArray = "0123456789abcdef".toCharArray()
+        val hexChars = CharArray(16 * 2)
+        bytes.indices.forEach {
+            val lowestByte = bytes[it].toInt() and 0xFF
+            hexChars[it * 2] = hexArray[lowestByte ushr 4]
+            hexChars[it * 2 + 1] = hexArray[lowestByte and 0x0F]
+        }
+        return String(
+            hexChars.sliceArray(0..7) + '-' +
+            hexChars.sliceArray(8..11) + '-' +
+            hexChars.sliceArray(12..15) + '-' +
+            hexChars.sliceArray(16..19) + '-' +
+            hexChars.sliceArray(20..31)
+        )
+    }
+
+    actual override fun toString(): String {
+        val hexArray = "0123456789abcdef".toCharArray()
+        val hexChars = CharArray(16 * 2 + 3)
+        hexChars[0] = 'x'
+        hexChars[1] = '\''
+        hexChars[16 * 2 + 2] = '\''
+        bytes.indices.forEach {
+            val lowestByte = (bytes[it] and 0xFF.toByte()).toInt()
+            hexChars[it * 2 + 2] = hexArray[lowestByte ushr 4]
+            hexChars[it * 2 + 3] = hexArray[lowestByte and 0x0F]
+        }
+        return String(hexChars)
+    }
+}
+
+actual val KClass<*>.className
+    get() = simpleName ?: "class_has_no_name"
